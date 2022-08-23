3d83c5066cd1e0a55c6ffaf305c80b85e2f6531c
yego <yegokipkemoi254@gmail.com>
2022-08-23 11:05:17 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+var customerName = "bob";
+function upperCaseCustomerName () {
+    return customerName = customerName.toUpperCase();
+}
+
+var bestCustomer;
+function setBestCustomer (){
+    return bestCustomer = "not bob";
+}
+function overwriteBestCustomer() {
+    return bestCustomer = "maybe bob";
+}
+const leastFvoriteCustomer = "brian"
+function changeLeastFavoriteCustomer () {
+    return leastFvoriteCustomer = "susan"
+}
